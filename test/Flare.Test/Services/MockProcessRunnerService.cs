a16d2543b98d60890c90a41effeacbce3ddb1a36
Byron Wheeler <bwheel1990@gmail.com>
2024-04-06 14:05:27 -0500

    update to .net 8.0

--- test/Flare.Test/Services/MockProcessRunnerService.cs
+++ test/Flare.Test/Services/MockProcessRunnerService.cs
@@ -2,7 +2,7 @@ using Flare.Services;
 
 namespace Flare.Test.Services
 {
-    public class MockProcessRunnerService : IProcessRunnerService
+    public class MockProcessRunnerService 
     {
     }
 }
